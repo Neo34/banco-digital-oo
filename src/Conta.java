8ba2de5a4024d301d90851735ad2eec3b85bcff9
Neo40 <tron31.cc@gmail.com>
2024-10-31 17:18:29 -0300

    Adição de Classe Abstrata para implementação

---
+++ src/Conta.java
@@ -0,0 +1,71 @@
+public abstract class Conta implements IConta {
+
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected Cliente cliente;
+
+    public Conta(Cliente cliente) {
+        if (cliente == null) {
+            throw new IllegalArgumentException("Cliente must not be null");
+        }
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.saldo = 0.0;
+        this.cliente = cliente;
+
+    }
+
+    @Override
+    public void sacar(double valor) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Valor de saque deve ser positivo");
+        }
+        if (valor > saldo) {
+            throw new IllegalArgumentException("Saldo insuficiente");
+        }
+        saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Valor de depósito deve ser positivo");
+        }
+        saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, IConta contaDestino) {
+        if (contaDestino == null) {
+            throw new IllegalArgumentException("Conta de destino não pode ser nula.");
+        }
+        if (valor == 0 || valor <= saldo) {
+            throw new IllegalArgumentException("Saldo Insuficiente.");
+        }
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    protected void imprimirInfosComuns() {
+        System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+        System.out.println(String.format("Agencia: %d", this.agencia));
+        System.out.println(String.format("Numero: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
+    }
+}
