2f7ee38feadb50fa5603b693492024b16b003bad
Neo40 <tron31.cc@gmail.com>
2024-11-01 18:59:26 -0300

    Adição de Conta Poupança

---
+++ src/ContaPoupanca.java
@@ -0,0 +1,30 @@
+/**
+ * Representa uma conta poupança que estende a classe base Conta.
+ */
+public class ContaPoupanca extends Conta {
+
+    /**
+     * Construtor que inicializa a ContaPoupanca com um cliente específico.
+     *
+     * @param cliente O cliente associado a esta conta poupança.
+     */
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
+    /**
+     * Imprime o extrato da conta poupança com detalhes comuns da conta.
+     * Sobrescreve o método imprimirExtrato da classe base.
+     */
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Poupança ===");
+
+        // Chama o método da superclasse para imprimir as informações comuns da conta.
+        try {
+            super.imprimirInfosComuns();
+        } catch (Exception e) {
+            System.err.println("Erro ao imprimir as informações da conta: " + e.getMessage());
+        }
+    }
+}
